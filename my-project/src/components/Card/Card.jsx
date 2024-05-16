de3fc07861b1eab31a77df670547ecab2d925c2c
vigneshp25 <vigneshwaranp25@gmail.com>
2024-05-16 14:15:02 +0530

    Ui changes

--- my-project/src/components/Card/Card.jsx
+++ my-project/src/components/Card/Card.jsx
@@ -3,7 +3,12 @@ import CardDetails from "./Card_Details";
 
 const Card = (props) => {
     const { linkText } = props;
-    const [showmore, setShowmore] = useState('tracking-wide py-1 h-[100px]');
+    const [showmore, setShowmore] = useState(Array(CardDetails.length).fill(true));
+    const toggleShowMore = (index) => {
+        const newShowmore = [...showmore];
+        newShowmore[index] = !newShowmore[index];
+        setShowmore(newShowmore);
+    }
     return (
         <div className="flex gap-8 mx-8">
             {CardDetails.map((card, index) => {
@@ -13,9 +18,9 @@ const Card = (props) => {
                     <div key={index} className="flex flex-col w-2/3">
                         <h3 className="text-xl uppercase">{heading}</h3>
                         <img src={image} alt={image} className="w-full h-[250px] py-2 flex" />
-                        <p className={showmore}>{content}</p>
+                        <p className={showmore[index] ? 'tracking-wide py-1 h-[100px] overflow-hidden' : 'tracking-wide py-1'}>{content}</p>
                         <a href="javascript:;" className="text-sm py-4 tracking-tight after:content-['_↗']" onClick={() => {
-                            setShowmore('tracking-wide py-1');
+                            toggleShowMore(index);
                         }}>
                             {cardLinkText}
                         </a>
