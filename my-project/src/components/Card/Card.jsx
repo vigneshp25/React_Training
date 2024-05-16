de8bdaa77483dbe255f2effd3724670308ab0cde
vigneshp25 <vigneshwaranp25@gmail.com>
2024-05-16 10:52:56 +0530

    ui changes

--- my-project/src/components/Card/Card.jsx
+++ my-project/src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import CardDetails from "./Card_Details";
 
 const Card = (props) => {
     const { linkText } = props;
+    const [showmore, setShowmore] = useState('tracking-wide py-1 h-[100px]');
     return (
         <div className="flex gap-8 mx-8">
             {CardDetails.map((card, index) => {
@@ -11,8 +13,10 @@ const Card = (props) => {
                     <div key={index} className="flex flex-col w-2/3">
                         <h3 className="text-xl uppercase">{heading}</h3>
                         <img src={image} alt={image} className="w-full h-[250px] py-2 flex" />
-                        <p className="tracking-wide py-1">{content}</p>
-                        <a href="javascript:;" className="text-sm py-4 tracking-tight after:content-['_↗']">
+                        <p className={showmore}>{content}</p>
+                        <a href="javascript:;" className="text-sm py-4 tracking-tight after:content-['_↗']" onClick={() => {
+                            setShowmore('tracking-wide py-1');
+                        }}>
                             {cardLinkText}
                         </a>
                     </div>
